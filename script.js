1d1cbbc5fb9076d9968dc4c9d7ce737005c25792
Jax Code <jax-code@444s-Air.attlocal.net>
2023-06-12 14:49:12 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// Repositories Fetch
+fetch("https://api.github.com/users/lily-fitz/repos")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// Repository Language Url Fetch
+fetch(
+  "https://api.github.com/repos/lily-fitz/classic-wordpress-theme-wordpress-scripts/languages"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// Repository Language Url Fetch w/ Object.keys
+fetch(
+  "https://api.github.com/repos/lily-fitz/classic-wordpress-theme-wordpress-scripts/languages"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(Object.keys(data));
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// Repository Language Url Fetch, adding languages to the page
+fetch(
+  "https://api.github.com/repos/lily-fitz/classic-wordpress-theme-wordpress-scripts/languages"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    Object.keys(data).forEach((language) => {
+      const newLanguageElement = document.createElement("p");
+      newLanguageElement.textContent = language;
+      document.body.appendChild(newLanguageElement);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
